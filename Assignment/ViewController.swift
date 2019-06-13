d97dafab5152c6f3c0ad249251b1fd4208c7fabe
Alina Hambaryan <alina_hambaryan@yahoo.com>
2019-06-13 10:54:03 +0200

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by ALINA HAMBARYAN on 6/13/19.
+//  Copyright © 2019 ah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
